ecfb2a00bc01e3221ce2c0067ef11784bfb7276c
caoheng01@live.com <caoheng@youlu.com>
2022-07-05 10:27:37 +0800

    feat: panic recover和go协程

--- src/main/go_recover_demo.go
+++ src/main/go_recover_demo.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// 假如defer语句
-	defer fmt.Println("deferred call in main")
+	/*defer fmt.Println("deferred call in main")
 	firstName := "yun chang"
 	fullNameRecover(&firstName, nil)
-	fmt.Println("returned normally from main")
+	fmt.Println("returned normally from main")*/
+
+	// panic recover 和 go 协程
+	testA()
 }
 
 /*
@@ -34,4 +40,28 @@ func recoverName() {
 	if r := recover(); r != nil {
 		fmt.Println("recovered from ", r)
 	}
+}
+
+//只有在相同的go协程中调用recover才管用，recover不能恢复一个不同协程的panic。
+//在下面的程序中，函数B发生了panic， 函数A调用了一个延迟函数recovery（）用于恢复panic。
+//但是函数B作为一个不同的协程来调用，下一行sleep只是保证A和B运行结束之后才退出
+//这个时候，panic是不能被恢复的，因为调用recovery的协程和B方法中发生panic的协程并不相同
+//如果把go testB() 修改为 testB() 就可以恢复panic了
+func recovery() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered: ", r)
+	}
+}
+
+func testA() {
+	defer recovery()
+	fmt.Println("inside A")
+	//go testB()
+	testB()
+	time.Sleep(1 * time.Second)
+}
+
+func testB() {
+	fmt.Println("inside B")
+	panic("B method panicked")
 }
