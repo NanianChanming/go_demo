66cf6e45cc5a8fb148c4adc1977ae98702a6d33f
Caoheng <caoheng@youlu.com>
2022-07-08 10:30:33 +0800

    feat: 分块读取文件及按行读取

--- src/main/go_read_file.go
+++ src/main/go_read_file.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
 	//readTest()
-	fptr := flag.String("fpath", "test.txt", "file path to read from")
-	flag.Parse()
-	fmt.Println("value of fpath is", *fptr)
-	readTestFlag(fptr)
+	//fptr := flag.String("fpath", "test.txt", "file path to read from")
+	//flag.Parse()
+	//fmt.Println("value of fpath is", *fptr)
+	//readTestFlag(fptr)
+	//readTestBuffer()
+	readTestScan()
 }
 
 /*
@@ -46,4 +50,43 @@ func readTestFlag(fptr *string) {
 		return
 	}
 	fmt.Println("file contents: ", string(file))
+}
+
+// 分块读取文件
+// 当文件非常大时，尤其在RAM存储量不足的情况下，把整个文件都读取到内存中是没有意义的。更好的方法是分块读取文件
+// 这可以使用bufio包来完成
+// 下面这个方法，以3个字节的块为单位读取test.txt文件
+func readTestBuffer() {
+	f, err := os.Open("D:/GoProjects/hello_go/src/resource/test.txt")
+	if err != nil {
+		fmt.Println("File reading error: ", err)
+		return
+	}
+	reader := bufio.NewReader(f)
+	b := make([]byte, 4)
+	for {
+		_, err := reader.Read(b)
+		if err != nil {
+			fmt.Println("reading file error: ", err)
+			break
+		}
+		fmt.Println(string(b))
+	}
+}
+
+//逐行读取
+func readTestScan() {
+	f, err := os.Open("D:/GoProjects/hello_go/src/resource/test.txt")
+	if err != nil {
+		fmt.Println("File reading error: ", err)
+		return
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	error := scanner.Err()
+	if err != nil {
+		log.Fatal(error)
+	}
 }
